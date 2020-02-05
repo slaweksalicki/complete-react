e7616028f4aa055aa156d3718c01f496be7f5817
Slawomir Salicki <slawomir.salicki@tangent.co.uk>
2020-02-05 17:13:03 +0000

    Firebase arrays

--- expensify-app/src/firebase/firebase.js
+++ expensify-app/src/firebase/firebase.js
@@ -14,31 +14,127 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().set(
-  {
-    name: 'Jon Snow',
-    age: 15,
-    stressLevel: 6,
-    job: {
-      title: 'Bastard',
-      company: 'Starks'
-    },
-    location: {
-      city: 'Winterfell',
-      country: 'North'
-    }
-  }
-).then(() => {
-  console.log('Data is saved');
-}).catch((error) => {
-  console.log('This failed.', error);
+//child_removed
+database.ref('expenses').on('child_removed', (snapshot) => {
+  console.log(snapshot.key, snapshot.val());
 });
 
-database.ref().update({
-  stressLevel: 9,
-  'job/company': 'Night Watch',
-  'location/city': 'The Wall'
+// child_changed
+database.ref('expenses').on('child_changed', (snapshot) => {
+  console.log(snapshot.key, snapshot.val());
 });
+
+// child_added
+database.ref('expenses').on('child_added', (snapshot) => {
+  console.log(snapshot.key, snapshot.val());
+});
+
+// database.ref('expenses')
+//   .once('value')
+//   .then((snapshot) => {
+//     const expenses = [];
+
+//     snapshot.forEach((childSnapshot) => {
+//       expenses.push({
+//         id: childSnapshot.key,
+//         ...childSnapshot.val()
+//       });
+//     });
+
+//     console.log(expenses);
+//   });
+
+
+// database.ref('expenses')
+//   .on('value', (snapshot) => {
+//     const expenses = [];
+
+//     snapshot.forEach((childSnapshot) => {
+//       expenses.push({
+//         id: childSnapshot.key,
+//         ...childSnapshot.val()
+//       });
+//     });
+
+//     console.log(expenses);
+//   });
+
+
+
+// database.ref('expenses').push({
+//   description: 'Wine',
+//   note: 'red wine from Spain',
+//   amount: 1200,
+//   createdAt: 45235234525,
+// });
+
+
+// database.ref('notes').push({
+//   title: 'Course topic',
+//   body: 'React native'
+// });
+
+// database.ref().on('value', (snapshot) => {
+//   const val = snapshot.val();
+//   console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+// })
+
+
+
+// const onValueChange = database.ref()
+//   .on('value', (snapshot) => {
+//     console.log(snapshot.val());
+//   }, (e) => {
+//     console.log('Error with data fetching', e);
+//   });
+
+// setTimeout(() => {
+//   database.ref('age').set(16);
+// }, 3500);
+
+// setTimeout(() => {
+//   database.ref().off(onValueChange);
+// }, 7000);
+
+// setTimeout(() => {
+//   database.ref('age').set(18);
+// }, 10500);
+
+// database.ref('location/city')
+//   .once('value')
+//   .then((snapshot) => {
+//     const val = snapshot.val();
+//     console.log(val);
+//   })
+//   .catch((e) => {
+//     console.log('Error fetching data', e)
+//   });
+
+// database.ref().set(
+//   {
+//     name: 'Jon Snow',
+//     age: 15,
+//     stressLevel: 6,
+//     job: {
+//       title: 'Bastard',
+//       company: 'Starks'
+//     },
+//     location: {
+//       city: 'Winterfell',
+//       country: 'North'
+//     }
+//   }
+// ).then(() => {
+//   console.log('Data is saved');
+// }).catch((error) => {
+//   console.log('This failed.', error);
+// });
+
+// database.ref().update({
+//   stressLevel: 9,
+//   'job/company': 'Night Watch',
+//   'location/city': 'The Wall'
+// });
 
 //database.ref('isSingle').set(null);
 
